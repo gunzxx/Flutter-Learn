aa1b0e3a150255ec8ddb7b5b9a7ba243a73b6324
gunzxx <ggm5850@gmail.com>
2023-04-01 11:54:44 +0700

    row column + warna

--- f_01/lib/main.dart
+++ f_01/lib/main.dart
@@ -8,12 +8,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("row column"),
+          title: const Text("Latihan row column"),
         ),
-        body: Center(
-          child: Text("Hello"),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Hello 1"),
+            Text("Hello 2"),
+            Text("Hello 3"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  child: Text("Hello 4"),
+                  color: Colors.blueAccent,
+                ),
+                Container(
+                  child: Text("Hello 5"),
+                  color: Colors.greenAccent,
+                ),
+                Container(
+                  child: Text("Hello 6"),
+                  color: Colors.redAccent,
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
