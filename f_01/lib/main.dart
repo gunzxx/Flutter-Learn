c457d4a2f6ba7adb0f8f102b1fe917cc2c3b606b
gunzxx <ggm5850@gmail.com>
2023-04-01 11:29:00 +0700

    text widget

--- f_01/lib/main.dart
+++ f_01/lib/main.dart
@@ -2,19 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
+int angka = 1;
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // darkTheme: true,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("row column"),
+          title: const Text("Ini navbar"),
         ),
         body: Center(
-          child: Text("Hello"),
-        ),
+            child: Container(
+          color: Colors.lightBlue,
+          width: 150,
+          // height: 50,
+          child: const Text(
+            "Hello world, saya sedang belajar flutter di android untuk mata kuliah pemrograman berbasis mobile",
+            // maxLines: 2,
+            overflow: TextOverflow.clip,
+            // softWrap: false,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        )),
       ),
     );
   }
