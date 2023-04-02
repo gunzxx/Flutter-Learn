766d6355da5e079dee3623a0de7860014c7d5400
gunzxx <ggm5850@gmail.com>
2023-04-02 20:37:54 +0700

    card widget

--- f_01/lib/main.dart
+++ f_01/lib/main.dart
@@ -1,6 +1,6 @@
 // import 'dart:math';
 
-import 'package:f_01/login_page.dart';
+// import 'package:f_01/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -12,38 +12,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color(0xFF1b1e23),
-            leading: Icon(
-              Icons.adb,
-              color: Colors.white,
-            ),
-            title: Text("App Bar Example"),
-            actions: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app)),
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          backgroundColor: Color(0xFF1b1e23),
+          title: Text("Card widget"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: ListView(
+            children: [
+              buildCard(Icons.account_box, "Account Box"),
+              buildCard(Icons.adb, "Serangga android"),
             ],
-            flexibleSpace: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF0096ff),
-                    Color(0xFF6610f2),
-                  ],
-                  begin: FractionalOffset.topLeft,
-                  end: FractionalOffset.bottomRight,
-                ),
-                image: DecorationImage(
-                  image: AssetImage("img/pattern.png"),
-                  fit: BoxFit.cover,
-                  repeat: ImageRepeat.repeat,
-                ),
-              ),
-            ),
           ),
         ),
+      ),
+    );
+  }
+
+  Card buildCard(IconData iconData, String teksBox) {
+    return Card(
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.all(5),
+            child: Icon(iconData),
+          ),
+          Text(teksBox),
+        ],
       ),
     );
   }
