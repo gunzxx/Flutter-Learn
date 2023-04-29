303f1e7b6deb3a0aa23df97b320225059f3e2f37
gunzxx <ggm5850@gmail.com>
2023-04-29 09:24:47 +0700

    clear

--- f_01/lib/main.dart
+++ f_01/lib/main.dart
@@ -12,50 +12,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Latihan InkWell"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Colors.amber),
-                    shape: MaterialStatePropertyAll(StadiumBorder())),
-                onPressed: () {},
-                child: Text("Button 1"),
-              ),
-              Material(
-                borderRadius: BorderRadius.circular(20),
-                // elevation: 2,
-                child: Container(
-                  width: 150,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      gradient: LinearGradient(
-                          colors: [Colors.purple, Colors.pink],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight)),
-                  child: Material(
-                    elevation: 4,
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.transparent,
-                    child: InkWell(
-                      splashColor: Colors.amber,
-                      borderRadius: BorderRadius.circular(20),
-                      onTap: () {},
-                      child: Center(
-                          child: Text(
-                        "Button 2",
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.w600),
-                      )),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
+        body: Center(),
       ),
     );
   }
